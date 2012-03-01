33eb3aa0278c77100cb9288fe2aec42931fc264b
Andrés Mejía <andmej@gmail.com>
2012-02-29 23:45:21 -0500

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Semillero::Application.config.secret_token = 'e237ed9406c20c05cc116852be41a593cd6c63a46b9e14aaa5f82e9932e47fe6b7380ad4d4a5775c222a99667b017a5342754cdfd5b5d36e6464ba4f0313505f'
